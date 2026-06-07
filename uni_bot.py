c148416b8c70ca63df48621a57b85d4a6e7adf86
rmkL0004 <reimki.0527@gmail.com>
2026-06-08 00:48:08 +0900

    Update uni_bot.py

--- uni_bot.py
+++ uni_bot.py
@@ -13,30 +13,33 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 def contains_uni(text):
-    text = unicodedata.normalize("NFKC", text)
+text = unicodedata.normalize("NFKC", text)
 
-    text = "".join(
-        chr(ord(c) - 0x60)
-        if "ァ" <= c <= "ヶ"
-        else c
-        for c in text
-    )
+```
+text = "".join(
+    chr(ord(c) - 0x60)
+    if "ァ" <= c <= "ヶ"
+    else c
+    for c in text
+)
 
-    return (
-        re.search(r"う\s*に", text)
-        or re.search(r"uni", text, re.IGNORECASE)
-    )
+return (
+    re.search(r"う\s*に", text)
+    or re.search(r"uni", text, re.IGNORECASE)
+)
+```
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+print(f"Logged in as {bot.user}")
 
 @bot.event
 async def on_message(message):
-    if message.author.bot:
-        return
+if message.author.bot:
+return
 
-    if contains_uni(message.content):
+```
+if contains_uni(message.content):
     responses = [
         "うにだ！",
         "うに！",
@@ -45,6 +48,7 @@ async def on_message(message):
 
     await message.reply(random.choice(responses))
 
-    await bot.process_commands(message)
+await bot.process_commands(message)
+```
 
 bot.run(TOKEN)
